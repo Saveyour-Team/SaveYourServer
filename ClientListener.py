6082e93940fcb068265e50fe6fd4aced54c2c339
Brendan Teich <teichb@gmail.com>
2015-03-01 01:09:10 -0500

    Fixed the skeleton code so that it runs and sends a simple "Hello World"ish response to any connections.

--- ClientListener.py
+++ ClientListener.py
@@ -1,36 +1,39 @@
 import threading
 import socket
 
-PORT = 13372
+PORT = 80 #Currently set to 80 so I can debug by going to localhost in my browser and seeing what it says.
 ADDRESS = ('127.0.0.1', PORT)
 MAX_INC_SIZE = 4096
 
-def getConnection(connectionSocket, addr):
+def getConnection(clientSocket, addr):
 	"""This method is called whenever a new connection thread is made."""
  
- 	clientRequest = connectionSocket.recv(MAKE_INC_SIZE)
+ 	clientRequest = clientSocket.recv(MAX_INC_SIZE)
 
- 	response = ''
+ 	response = 'Hello World! But seriously this is a placeholder.'
+ 	#print response;
  		
- 	connectionSocket.send(response)
- 	connectionSocket.close()
+ 	clientSocket.send(response)
+ 	clientSocket.close()
 
 
 
-def startListening:
+def startListening():
 	"""Start listening for incoming connections"""
 	#Prepare a sever socket
-	serverSocket = socket(AF_INET, SOCK_STREAM)
+	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	serverSocket.bind(ADDRESS)
 	serverSocket.listen(5)
 	while True:
 		#Establish the connection and spawn a new thread for each connection
- 		connectionSocket, addr = serverSocket.accept()
- 		thread = threading.Thread(None, getConnection, None, (connectionSocket, addr), {})
+ 		clientSocket, addr = serverSocket.accept()
+ 		thread = threading.Thread(None, getConnection, None, (clientSocket, addr), {})
  		thread.start()
 
-def stopListening:
+def stopListening():
 	"""Stop the server from listening for incoming connections."""
 	serverSocket.close() 
  	
 
+#This line is here for debug purposes, later on the ClientListener will probably be called from elsewhere.
+startListening()
